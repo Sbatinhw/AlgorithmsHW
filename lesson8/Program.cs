b12e65d77df738380f436b56d3ae8e5497ac04d3
Сергей <saygon707@gmail.com>
2021-07-03 17:57:38 +0300

    правки

--- lesson8/Program.cs
+++ lesson8/Program.cs
@@ -13,11 +13,13 @@ namespace AlgorithmHW
 
             //test = new int[] { 1, -4, -5}; //верно
 
-            //test = RandomArray(15); //верно
+            //test = RandomArray(); //верно
 
             //test = new int[] { 5, 5 }; //верно
 
             //test = new int[] { 1, -4, -5, 0, 2, 1 }; //верно
+
+            //test = new int[] { 5, 5, 3, 0 }; //верно
 
 
 
@@ -29,24 +31,27 @@ namespace AlgorithmHW
 
         public static int[] BuckSort(int[] arr)
         {
-            //
-            Console.WriteLine("tst");
-            Console.ReadLine();
-            //
+            //количество блоков
             int quant = Convert.ToInt32(Math.Sqrt(arr.Length));
 
             if(quant * quant != arr.Length) { quant += 1; }
 
+            //минимальное значение в массиве
             int min = SearchMin(arr);
+            //максимальное значение в массиве
             int max = SearchMax(arr);
+
             int len = max - min;
+            //диапазон для значений блоков
             int step = 0;
 
             if (len / quant < 1) { step = 0; }
             else if (len % quant != 0) { step = (len / quant) + 1; }
             else { step = len / quant; }
 
+            //список отсортированных элементов
             List<int> EndPart = new List<int>();
+            //список блоков
             List<List<int>> list = new List<List<int>>();
 
             for(int i = 0; i < quant; i++)
@@ -54,12 +59,17 @@ namespace AlgorithmHW
                 list.Add(new List<int>());
             }
 
+            //перебор и сортировка элементов изначального массива
             for(int i = 0; i < arr.Length; i++)
             {
                 int val = arr[i];
+
+                //минимальное значение блока
                 int minEdge = min;
+                //максимальное значение блока
                 int maxEdge = minEdge + step;
 
+                //добавление элемента в блок
                 for(int j = 0; j < quant; j++)
                 {
                     if(val >= minEdge && val <= maxEdge)
@@ -67,11 +77,12 @@ namespace AlgorithmHW
                         list[j].Add(val);
                         break;
                     }
+                    //переход к следующему блоку если не удаётся вставить элемент
                     else
                     {
                         minEdge = maxEdge + 1;
                         maxEdge = minEdge + step - 1;
-
+                        //костыль если массив состоит из одинаковых значений или значения отличаются на единицу
                         if(maxEdge < minEdge)
                         {
                             maxEdge = minEdge;
@@ -82,6 +93,8 @@ namespace AlgorithmHW
 
             for(int i = 0; i < quant; i++)
             {
+                //блок отправляется в рекурсивную сортировку если состоит больше чем из одного элемента
+                //и состоит из не одинаковых значений
                 if(list[i].Count > 1 && step != 0) 
                 { 
                     EndPart.AddRange(BuckSort(list[i].ToArray())); 
@@ -96,10 +109,10 @@ namespace AlgorithmHW
 
         }
 
-        public static int[] RandomArray(int maxlen = 20, int min = -10000, int max = 10000)
+        public static int[] RandomArray(int min = -10000, int max = 10000)
         {
             Random rand = new Random();
-            int[] arr = new int[rand.Next(1, maxlen)];
+            int[] arr = new int[20];
 
             for(int i = 0; i < arr.Length; i++)
             {
